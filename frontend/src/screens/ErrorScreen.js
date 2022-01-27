411e4479fc52eb50f136b9678554b737b475e60d
Adaji Mukhtar <alfadaji@gmail.com>
2022-01-27 12:20:15 +0100

    added routing the the pages

---
+++ frontend/src/screens/ErrorScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ErrorScreen = () => {
+  return <div className='errorScreen'>
+      <h1 className='text-uppercase text-center'>Error! page not found</h1>
+  </div>;
+};
+
+export default ErrorScreen;
