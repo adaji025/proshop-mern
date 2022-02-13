91b1b2a335909d8a4c08afed07df2547ae4ff874
Adaji Mukhtar <alfadaji@gmail.com>
2022-02-13 19:28:34 +0100

    implemented Loader and Message as a product

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux'
 import { Col, Row } from "react-bootstrap";
 import Product from "../components/Product";
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 import { listProduct } from "../actions/productActions";
 
 
@@ -19,7 +21,7 @@ const {  loading, err, products } = productList
     <>
       <h1>Latset Products</h1>
 
-      {loading ? <h2>Loading...</h2> : err ? <h3>{err}</h3> :
+      {loading ? <Loader /> : err ? <Message  variant='danger'>{err}</Message> :
       <Row>
         {products.map((product) => {
           return(
